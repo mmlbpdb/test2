36c652c30f249971be9df3fbe4780e52bfe3b675
mmlbpdb <mmlbpdb@skyline.be>
2022-11-30 07:45:31 +0100

    test

---
+++ TestAutomation_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAutomation_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("TestAutomation_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("CCE35FFC-82C0-405C-80B1-12B6136CA889")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ TestAutomation_1Tests/UtilityTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Script.Tests
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass()]
+	public class UtilityTests
+	{
+		[TestMethod()]
+		public void ToUpperTest()
+		{
+			// Act
+			var output = Utilities.Utility.ToUpper("a");
+
+
+
+			// Assert
+			Assert.AreEqual("A", output);
+		}
+	}
+}
